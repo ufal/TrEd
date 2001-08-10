a460eaa99dbe81628157e9a64fdfadbea8e34661
Petr Pajas <jan.stepanek@matfyz.cz>
2001-08-10 15:57:12 +0000

    Minor changes.

--- tredlib/Tk/BindMouseWheel.pm
+++ tredlib/Tk/BindMouseWheel.pm
@@ -3,13 +3,24 @@ package Tk::BindMouseWheel;
 # This module actually only adds features to Tk::Widget
 
 sub Tk::Widget::BindMouseWheelVert {
-    my($w,$modifier)= @_;
+    my($w,$modifier,@tags)= @_;
     $modifier.="-" if ($modifier);
-    $w->bind("<$modifier"."MouseWheel>",
-              [ sub { $_[0]->yview('scroll',-($_[1]/120)*3,'units') }, Tk::Ev("D")]);
+    $w->bind($tags,"<$modifier"."MouseWheel>",
+              [ sub { 
+		  $_[1]->yview('scroll',-($_[2]/120)*3,'units') 
+		}, $w, Tk::Ev("D")
+	      ]);
     if ($Tk::platform eq 'unix') {
-        $w->bind("<$modifier"."4>", sub { $_[0]->yview('scroll', -3, 'units') unless $Tk::strictMotif; });
-        $w->bind("<$modifier"."5>", sub { $_[0]->yview('scroll', 3, 'units') unless $Tk::strictMotif; });
+        $w->bind(@tags,"<$modifier"."4>",
+		 [sub {
+		    $_[1]->yview('scroll',-3,'units')
+		      unless $Tk::strictMotif;
+		  },$w]);
+        $w->bind(@tags,"<$modifier"."5>",
+		 [sub {
+		    $_[1]->yview('scroll', 3, 'units')
+		      unless $Tk::strictMotif;
+		  },$w]);
     }
 }
 
