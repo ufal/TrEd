a3ede7fe682d8984e3e7ed54334574f17a26eb74
Petr Pajas <pajas@ufal.mff.cuni.cz>
2001-10-26 16:59:20 +0000

    initial commit

---
+++ tredlib/TrEd/CPConvert.pm
@@ -0,0 +1,38 @@
+# -*- cperl -*-
+
+package TrEd::CPConvert;
+
+use Text::Iconv;
+
+sub new {
+  my ($self, $encoding1, $encoding2)=@_;
+  my $class = ref($self) || $self;
+  print ($encoding1, $encoding2,"\n");
+  my $conv1 = Text::Iconv->new($encoding1, $encoding2);
+  my $conv2 = Text::Iconv->new($encoding2, $encoding1);
+  return undef unless ($conv1 and $conv2);
+  my $new = bless [$conv1,$conv2,$encoding1,$encoding2], $class;
+  return $new;
+}
+
+sub encode {
+  my ($self,$string)=@_;
+  return $self->[1]->convert($string);
+}
+
+sub decode {
+  my ($self,$string)=@_;
+  return $self->[0]->convert($string);
+}
+
+sub encoding_to {
+  my ($self)=@_;
+  return $self->[2];
+}
+
+sub decoding_to {
+  my ($self)=@_;
+  return $self->[3];
+}
+
+1;
