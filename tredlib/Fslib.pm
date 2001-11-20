9629b023d1ad7851399808350217b37efb5eb6f6
Petr Pajas <pajas@ufal.mff.cuni.cz>
2001-11-20 10:17:44 +0000

    No change.

--- tredlib/Fslib.pm
+++ tredlib/Fslib.pm
@@ -1,7 +1,7 @@
 #
 # Revision: $Revision$
 # Checked-in: $Date$
-# Time-stamp: <2001-11-20 11:15:03 pajas>
+# Time-stamp: <2001-11-20 11:15:21 pajas>
 # See the bottom of this file for the POD documentation. Search for the
 # string '=head'.
 
