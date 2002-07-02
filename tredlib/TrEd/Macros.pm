3c8a048421cb6e82925b5ad515b927f46130ffcf
Petr Pajas <pajas@ufal.mff.cuni.cz>
2002-07-02 19:13:06 +0000

    some simple debug prints removed

--- tredlib/TrEd/Macros.pm
+++ tredlib/TrEd/Macros.pm
@@ -229,11 +229,9 @@ sub do_eval_hook {
   my ($win,$context,$hook)=(shift,shift,shift);  # $win is a reference
 				# which should in this way be made visible
 				# to hooks
-  print STDERR "about to run a hook $hook\n" if $hookDebug;
+  print STDERR "about to run the hook: '$hook'\n" if $hookDebug;
   $TredMacro::grp=$win;
-  print STDERR "testing $hook and $TredMacro::this\n" if $hookDebug;
   return undef unless $hook; # and $TredMacro::this;
-  print STDERR "no problem, continuing\n" if $hookDebug;
 
   unless ($macrosEvaluated) {
     eval (join("",@macros)."\n return 1;");
