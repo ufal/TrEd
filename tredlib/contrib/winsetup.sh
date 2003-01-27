b966a7acf2271250ef18d30087e40247bb3a5eca
Petr Pajas <jan.stepanek@matfyz.cz>
2003-01-27 08:38:47 +0000

    addedd  XML::LibXML::Iterator

--- tredlib/contrib/winsetup.sh
+++ tredlib/contrib/winsetup.sh
@@ -217,7 +217,7 @@ fi
 #    fi
 
 #  upgrade_package XML::Parser XML::DOM
-  upgrade_packages Tk Text::Iconv XML::SAX XML::LibXML Tie::IxHash
+  upgrade_packages Tk Text::Iconv XML::SAX XML::LibXML XML::LibXML::Iterator Tie::IxHash
 
 #else
 #  if [ "${TKTEST}" != "1" ]; then
