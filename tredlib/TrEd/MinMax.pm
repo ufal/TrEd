135ff45d74b09fc75cdc01487fc4a153c0be4a97
Petr Pajas <pajas@ufal.mff.cuni.cz>
2003-10-08 07:58:23 +0000

    - minmax

--- tredlib/TrEd/MinMax.pm
+++ tredlib/TrEd/MinMax.pm
@@ -2,32 +2,76 @@ package TrEd::MinMax;
 
 #
 # $Revision$ '
-# Time-stamp: <2001-06-04 08:49:10 pajas>
+# Time-stamp: <2003-10-06 17:17:03 pajas>
 #
 # Copyright (c) 2001 by Petr Pajas <pajas@matfyz.cz>
 # This software covered by GPL - The General Public Licence
 #
+require Exporter;
 
-use strict;
+@ISA=qw(Exporter);
+$VERSION = "0.1";
+@EXPORT = qw(min max);
+@EXPORT_OK=qw(first min max minstr maxstr reduce sum shuffle);
+eval 'require List::Util; die if List::Util::min(0,-1)!=-1; import List::Util qw(first min max minstr maxstr reduce sum shuffle)';
 
-BEGIN {
-  use Exporter  ();
-  use vars      qw($VERSION @ISA @EXPORT @EXPORT_OK);
-  @ISA=qw(Exporter);
-  $VERSION = "0.1";
-  @EXPORT = qw(&min &max);
-  @EXPORT_OK=();
+eval <<'ESQ' if $@ or not defined &reduce;
+
+# This code is only compiled if List::Util did not load
+
+use vars qw($a $b);
+
+sub reduce (&@) {
+  my $code = shift;
+
+  return shift unless @_ > 1;
+
+  my $caller = caller;
+  local(*{$caller."::a"}) = \my $a;
+  local(*{$caller."::b"}) = \my $b;
+
+  $a = shift;
+  foreach (@_) {
+    $b = $_;
+    $a = &{$code}();
+  }
+
+  $a;
 }
 
-sub min {
-  my ($a,$b)=@_;
-  return ($a<$b)?$a:$b;
+sub sum (@) { reduce { $a + $b } @_ }
+
+sub min (@) { reduce { $a < $b ? $a : $b } @_ }
+
+sub max (@) { reduce { $a > $b ? $a : $b } @_ }
+
+sub minstr (@) { reduce { $a lt $b ? $a : $b } @_ }
+
+sub maxstr (@) { reduce { $a gt $b ? $a : $b } @_ }
+
+sub first (&@) {
+  my $code = shift;
+
+  foreach (@_) {
+    return $_ if &{$code}();
+  }
+
+  undef;
 }
 
-sub max {
-  my ($a,$b)=@_;
-  return ($a<$b)?$b:$a;
+sub shuffle (@) {
+  my @a=\(@_);
+  my $n;
+  my $i=@_;
+  map {
+    $n = rand($i--);
+    (${$a[$n]}, $a[$n] = $a[$i])[0];
+  } @_;
 }
+
+ESQ
 
 1;
+
+
 
