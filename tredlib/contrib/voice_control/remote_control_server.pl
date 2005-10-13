01a0eb185e3c539c519637b6df98e91de161f7c8
Petr Pajas <pajas@ufal.mff.cuni.cz>
2005-10-13 19:00:36 +0000

    updated remote control stuff

--- tredlib/contrib/voice_control/remote_control_server.pl
+++ tredlib/contrib/voice_control/remote_control_server.pl
@@ -1,10 +1,32 @@
-#!/usr/bin/perl -Tw
+#!/usr/bin/perl
 
 use strict;
-BEGIN { $ENV{PATH} = '/usr/ucb:/bin' }
 use Socket;
 use IO::Socket;
 use Carp;
+
+use FindBin;
+use lib ("$FindBin::RealBin", "$FindBin::RealBin/../lib",
+         "$FindBin::Bin","$FindBin::Bin/../lib",
+	 "$FindBin::Bin/lib", "$FindBin::RealBin/lib"
+	);
+
+
+use Getopt::Long;
+use vars qw($shell $OUT $term);
+GetOptions(
+  "shell|s" => \$shell
+ );
+
+if ($shell) {
+  $ENV{PERL_READLINE_NOWARN}=1;
+  require Term::ReadLine;
+  $term = new Term::ReadLine('control');
+  $OUT = $term->OUT;
+  print $OUT "SHELL: ",$term->ReadLine,"\n";
+} else {
+  $OUT = \*STDOUT;
+}
 
 my $EOL = "\015\012";
 
@@ -34,16 +56,45 @@ while (($new_sock,$peer_addr) = $sock->accept()) {
 
   logmsg "connection from $name [", $new_sock->peerhost(), "] at port ".$new_sock->peerport();
 
-  while (defined($_=<STDIN>) and not /disconnect_client/) {
-    chomp $_;
-    print $new_sock $_,$EOL;
-    print STDERR "Echo: $_\n";
-    $new_sock->flush();
+  if ($shell) {
+    print $OUT "SHELL: ",$term->ReadLine,"\n";
+    while (defined($_=get_line($term,'> ','')) and not /^\\disconnect_client/) {
+      exit 0 if (/^\\quit/);
+      chomp $_;
+      print $new_sock $_,$EOL;
+      print $OUT "Echo: $_\n";
+      $new_sock->flush();
+    }
+  } else {
+    print $OUT "NO SHELL\n";
+    while (defined($_=<STDIN>) and not /^\\disconnect_client/) {
+      exit 0 if (/^\\quit/);
+      chomp $_;
+      print $new_sock $_,$EOL;
+      print STDERR "Echo: $_\n";
+      $new_sock->flush();
+    }
   }
-  logmsg "end of communication with $name [", $new_sock->peerhost(), "] at port ".$new_sock->peerport();
+    logmsg "end of communication with $name [", $new_sock->peerhost(), "] at port ".$new_sock->peerport();
+}
+
+sub get_line {
+  my ($term,$prompt,$retonint)=@_;
+  my $line;
+  $SIG{INT}=sub { die 'TRAP-SIGINT'; };
+  eval {
+    $line = $term->readline($prompt);
+  };
+  if ($@) {
+    if ($@ =~ /TRAP-SIGINT/) {
+      print "\n" unless $term->ReadLine eq 'Term::ReadLine::Perl'; # clear screen
+      return $retonint;
+    } else {
+      print STDERR $@,"\n";
+      return undef;
+    }
+  }
+  return $line;
 }
 
 close ($sock);
-
-
-
