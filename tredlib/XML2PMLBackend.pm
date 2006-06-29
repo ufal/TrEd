8eb420c490e973799fcdd316a14f4a009c0fd3d3
Petr Pajas <jan.stepanek@matfyz.cz>
2006-06-29 11:31:59 +0000

    minor cleanup, add support for output stylesheet

--- tredlib/XML2PMLBackend.pm
+++ tredlib/XML2PMLBackend.pm
@@ -1,33 +1,52 @@
 package XML2PMLBackend;
 
 use IOBackend qw(set_encoding);
+use Exporter;
 use base qw(PMLBackend);
 
 use strict;
-use vars qw($xslt_processor $xslt_stylesheet $xslt_processor_opts $encoding);
+use vars qw($xslt_processor $xslt_processor_opts $in_stylesheet $out_stylesheet $in_stylesheet_path $out_stylesheet_path @EXPORT_OK);
 
 sub default_settings {
   $xslt_processor = "xsltproc" unless $xslt_processor;
   $xslt_processor_opts = "%s -" unless $xslt_processor_opts;
-  $xslt_stylesheet = Fslib::FindInResources("xml2xdata.xsl") unless $xslt_stylesheet;
-  $encoding = 'utf-8';
+  $in_stylesheet ||= "xml2xdata.xsl";
+  $in_stylesheet_path ||= Fslib::FindInResources($in_stylesheet);
+  $out_stylesheet ||= "xdata2xml.xsl";
+  $out_stylesheet_path ||= Fslib::FindInResources($out_stylesheet);
 }
+
+BEGIN {
+    @EXPORT_OK = qw(&test &test_xslt_processor &xslt_commandline &default_settings
+		    &open_backend &close_backend &read &write);
+  default_settings();
+  print STDERR "xslt processor $xslt_processor\n" if $Fslib::Debug;
+};
+
+
+
+sub xslt_commandline {
+  my ($cmd, $stylesheet) = @_;
+
+  print STDERR "$cmd\n" if $Fslib::Debug;
+  $cmd=~s/\%s/$in_stylesheet_path/g;
+  $cmd=~s/\%f/-/g;
+  return $cmd;
+}
+
+*xslt_commandline_in = \&xslt_commandline;
+*xslt_commandline_out = \&xslt_commandline;
 
 sub open_backend {
   my ($filename, $mode, $encoding)=@_;
   if ($mode eq 'w') {
-    return IOBackend::open_backend($filename,$mode,$encoding);
+    my $cmd = xslt_commandline_in($xslt_processor." ".$xslt_processor_opts,$in_stylesheet_path);
+    print STDERR "[r $cmd]\n" if $Fslib::Debug;
+    my $fh = IOBackend::open_pipe($filename,'w',$cmd);
   } elsif ($mode eq 'r') {
-    my $fh = undef;
-    my $cmd = $xslt_processor." ".$xslt_processor_opts;
-    print STDERR "$cmd\n" if $Fslib::Debug;
-    $cmd=~s/\%s/$xslt_stylesheet/g;
-    $cmd=~s/\%f/-/g;
-    print STDERR "[r $cmd]\n"; # if $Fslib::Debug;
-    no integer;
-#    $fh = set_encoding(
-    $fh = IOBackend::open_pipe($filename,'r',$cmd);
-      #,$encoding);
+    my $cmd = xslt_commandline_out($xslt_processor." ".$xslt_processor_opts,$out_stylesheet_path);
+    print STDERR "[r $cmd]\n" if $Fslib::Debug;
+    my $fh = IOBackend::open_pipe($filename,'r',$cmd);
   } else {
     die "unknown mode $mode\n";
   }
@@ -41,33 +60,26 @@ sub open_backend {
 sub test_xslt_processor {
   return 1 if (-x $xslt_processor);
   foreach (split(($^O eq 'MSWin32' ? ';' : ':'),$ENV{PATH})) {
-    print STDERR "xslt processor not found at $xslt_processor\n";# if $Fslib::Debug;
     return 1 if -x "$_".($^O eq 'MSWin32' ? "\\" : "/")."$xslt_processor";
   }
-  print STDERR "xslt processor not found at $xslt_processor\n";# if $Fslib::Debug;
+  print STDERR "xslt processor not found at $xslt_processor\n" if $Fslib::Debug;
   return 0;
 }
 
 sub test {
   my ($f,$encoding)=@_;
-  print STDERR "TESTING $f\n";# if $Fslib::Debug;
   return 0 unless test_xslt_processor();
-  print STDERR "FOUND xslt processor $xslt_processor\n";# if $Fslib::Debug;
   if (ref($f)) {
     local $_;
     1 while ($_=$f->getline() and !/\S/);
     return 0 unless (/^\s*<\?xml\s/);
+    return 1;
   } else {
     my $fh = IOBackend::open_backend($f,"r");
     my $test = $fh && test($fh,$encoding);
     IOBackend::close_backend($fh);
     return $test;
   }
-}
-
-BEGIN {
-  default_settings();
-  print STDERR "xslt processor $xslt_processor\n";# if $Fslib::Debug;
 }
 
 1;
