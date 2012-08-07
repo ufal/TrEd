58bf7699602a1d61126773a5b45f49eaceedf5e7
Martin Kruliš <krulis@ufal.mff.cuni.cz>
2012-08-07 09:24:08 +0000

    OS X Releasing templates and scripts added.

---
+++ osx_install/scripts/upload.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SAVE_DIR=`pwd`
+cd `dirname $0` || exit 1
+
+. .config
+
+
+# Get Login Name
+echo "Uploading $RELEASE_FILE to $UPLOAD_DEST ..."
+echo -n "Login: "; read LOGIN
+if [ -z "$LOGIN" ]; then
+    echo "Login name is empty!"
+    exit 1
+fi
+
+
+# Upload
+scp "$RELEASE_FILE" "${LOGIN}@$UPLOAD_DEST"
+
+
+# Cleanup
+cd "$SAVE_DIR"
