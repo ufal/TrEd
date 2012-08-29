10d639e865f89142a955af5e40836c358fbb35a8
Martin Kruliš <krulis@ufal.mff.cuni.cz>
2012-08-29 18:14:57 +0000

    Make file for TrEd streamlined and purged of all non-essential stuff. Admin scripts updated accordingly.

--- admin/build-dep-package.sh
+++ admin/build-dep-package.sh
@@ -5,6 +5,7 @@ EXTDIR=`dirname $(readlink -fen $0)`
 . "$EXTDIR"/env.sh
 
 echo "Building a package with unix TrEd dependencies" && \
+mkdir -p "${WWW}/tred" && \
 # create package with tred unix dependencies
 tar czhf "${WWW}/tred/tred-dep-unix.tar.gz" --exclude=.svn --exclude='*~' -C ${TRED_UNIXINST_DIR} packages_unix && \
 # create linux installation script in $WWW/tred directory
--- admin/print-make-usage.sh
+++ admin/print-make-usage.sh
@@ -11,15 +11,13 @@ echo "  make release             - install + build distribution packages and upl
 echo "  make test                - connect to testing infrastructure and perform TrEd tests;"
 echo "                             the TrEd must be already released to testbed ($(REMOTE_WWW))"
 echo "  make publish             - upload TrEd release from testbed to its official web site"
-echo 
-echo "  make release-tred-qcmd   - like release but uses non-interactive SGE jobs rather than qrsh"
-echo "  make update-dist-dir     - only update TrEd distribution tree in $(DIST_DIR)"
-echo "  make update-dep-packages - fetch latest versions of required modules and libraries"
-echo "  make release-dep-package - only release tred dependency package to remote server ($(REMOTE_WWW))"
-echo "  make new-treex-pml       - create and install new Treex::PML packages"
-echo 
-echo "  make sync-www            - rsync WWW source tree with the testbed ($(REMOTE_WWW))"
-echo 
+echo "  make testbed-status      - connect to testbed server and check its status"
+echo "  make testbed-clear       - clear snapshots from the testbed after testing"
+echo
+echo
 echo "Example: make release test publish   - a fast way how to create a new public TrEd release"
 echo
+echo "Notes:"
+echo "  - It's better to check testbed status before every testing."
+echo "  - It's polite to clear testbed after successful testing."
 echo
