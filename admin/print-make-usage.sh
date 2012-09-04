bca1151f63755160ed3323f42a0288c8ef172a31
Jan Štěpánek <jan.stepanek@matfyz.cz>
2012-09-04 16:02:07 +0000

    - Makefile syntax converted to shell syntax - fixed typo

--- admin/print-make-usage.sh
+++ admin/print-make-usage.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-EXTDIR=`diname $(readlink -fen $0)`
+EXTDIR=`dirname $(readlink -fen $0)`
 . $EXTDIR/env.sh
 
 
 echo "You can:"
 echo 
-echo "  make install             - install TrEd and extensions into $(INSTALL_BASE)"
-echo "  make release             - install + build distribution packages and upload to the testbed ($(REMOTE_WWW))"
+echo "  make install             - install TrEd and extensions into $INSTALL_BASE"
+echo "  make release             - install + build distribution packages and upload to the testbed ($REMOTE_WWW)"
 echo "  make test                - connect to testing infrastructure and perform TrEd tests;"
-echo "                             the TrEd must be already released to testbed ($(REMOTE_WWW))"
+echo "                             the TrEd must be already released to testbed ($REMOTE_WWW)"
 echo "  make publish             - upload TrEd release from testbed to its official web site"
 echo "  make testbed-status      - connect to testbed server and check its status"
 echo "  make testbed-clear       - clear snapshots from the testbed after testing"
