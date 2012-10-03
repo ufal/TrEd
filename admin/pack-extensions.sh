db1c3e4b9f6f72d771a2b4c48a9c3c16d4a286a4
Martin Kruliš <krulis@ufal.mff.cuni.cz>
2012-10-03 14:00:56 +0000

    Extensions repository transfer finalized.

--- admin/pack-extensions.sh
+++ admin/pack-extensions.sh
@@ -9,10 +9,16 @@ echo "Packing TrEd extensions" && \
 cd ${TRED_EXT_DIR} && ./make && \
 
 ##TODO This script also prepares extensions in $WWW/tred/extensions, one icon is missing there, maybe put it into svn later
-cd ${WWW}/tred/extensions/core && \
-wget -nv http://ufal.mff.cuni.cz/tred/extensions/extension.png && \
+cd ${WWW}/tred/extensions && \
+#wget -nv http://ufal.mff.cuni.cz/tred/extensions/extension.png && \
 ##TODO pdt20 doc is created in wrong directory, this is a temporary fix
 mv -f ${WWW}/pdt* ${WWW}/tred/documentation 
 rm -f ${WWW}/pdt*
+
+# Copy indexing script to extensions release dir
+FILES='index.php extension.png'
+for  FILE in $FILES ; do
+	cp -f ${TRED_EXT_DIR}/$FILE ${WWW}/tred/extensions/$FILE
+done
 
 echo "Packing TrEd extensions done."
