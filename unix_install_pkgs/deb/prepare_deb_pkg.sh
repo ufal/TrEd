08f1a9b29f8a089176f1e4b85892e0d0f3183db7
Martin Kruliš <krulis@ufal.mff.cuni.cz>
2012-10-23 13:34:45 +0000

    The deb-package building script tested and finalized.

--- unix_install_pkgs/deb/prepare_deb_pkg.sh
+++ unix_install_pkgs/deb/prepare_deb_pkg.sh
@@ -7,17 +7,23 @@ SVN_VERSION=`svn info . | grep 'Revision:' | sed -E 's/[^0-9]+//g'`
 
 
 # Prepare fresh checkout
+echo "Export package config dir from SVN so we can work with it ..."
 TRED_DEB_DIR="./tred-2.$SVN_VERSION"
 svn export ./tred-2.0 "$TRED_DEB_DIR" || exit 2
 
 # Fix changelog
+echo "Fix version and release date in the configuration ..."
 DATE=`date '+%a, %d %b %Y %T %z'`
 sed -i "s/%SVN_VERSION%/${SVN_VERSION}/g" "$TRED_DEB_DIR/debian/changelog" || exit 3
 sed -i "s/%DATE%/${DATE}/g" "$TRED_DEB_DIR/debian/changelog" || exit 3
 
 # Build the package
+echo "Build the deb package ..."
+cd "$TRED_DEB_DIR" || exit 4
 debuild -us -uc || exit 4
+cd .. || exit 4
 
 # Cleanup
+echo "Cleaning up ..."
 rm -rf "$TRED_DEB_DIR"
 cd "$SAVE_DIR"
