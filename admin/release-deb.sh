da845209c1cfbd83088d262679438d6902708890
Martin Kruliš <krulis@ufal.mff.cuni.cz>
2012-10-25 11:24:15 +0000

    Deb pakcage creation integrated into standard release process.

---
+++ admin/release-deb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Prepare deb package and upload it to testbed website.
+# The core release must be made first, so the install_tred.bash script is ready.
+
+EXTDIR=`dirname $(readlink -fen $0)`
+. $EXTDIR/env.sh
+
+SVN_VERSION=`svn info . | grep 'Revision:' | sed -E 's/[^0-9]+//g'`
+
+# Prepare the deb package
+cd "$PROJECT_DIR/unix_install_pkgs/deb" || exit 1
+./prepare_deb_pkg.sh || exit 2
+
+# Upload the package to the testbed website
+echo "Uploading the package to testbed web ..."
+DEB_FILE="tred_2.${SVN_VERSION}_all.deb"
+scp "./${DEB_FILE}" "${LOGIN_NAME}@${REMOTE_WWW}/${DEB_FILE}"
+
+# Make sure tred.deb link points to the newest deb package
+echo "Creating symling tred.deb ..."
+ssh ${LOGIN_NAME}@${TESTING_SERVER} "cd /var/www/tred/testbed && ln -sf ./${DEB_FILE} ./tred.deb"
+
+cd "$EXTDIR"
