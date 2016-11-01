cd8924bf0a21adb1a02a3650e7ed826a8863d344
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2016-11-01 15:41:11 +0000

    trying to fix wrong permissions to /Applications

--- osx_install/scripts/make-release.sh
+++ osx_install/scripts/make-release.sh
@@ -26,14 +26,14 @@ echo "Removing old files ..."
 if [ -z  $INSTALL_SCRIPT_URL ]; then
 	rm -rf "$INSTALL_SCRIPT"
 fi
-rm -rf "/Applications/$TRED_INSTALL_APP"
+rm -rf "$APPLICATIONS/$TRED_INSTALL_APP"
 rm -rf "$MOUNT_POINT/$TRED_DIR"
 rm -rf "$MOUNT_POINT/$TRED_APP/Contents/_CodeSignature"
 
 
 # Prepare fresh template
-echo "Copying template contents to the /Applications/$TRED_INSTALL_APP ..."
-cp -R "$MOUNT_POINT/$TRED_APP" "/Applications/$TRED_INSTALL_APP" || exit 2
+echo "Copying template contents to the $APPLICATIONS/$TRED_INSTALL_APP ..."
+cp -R "$MOUNT_POINT/$TRED_APP" "$APPLICATIONS/$TRED_INSTALL_APP" || exit 2
 
 
 # Get TrEd install script
@@ -47,27 +47,27 @@ else
 	exit 3
 fi
 
-# Install TrEd to /Applications from bash script
-echo "Installing TrEd to /Applications/$TRED_DIR ..."
+# Install TrEd to $APPLICATIONS from bash script
+echo "Installing TrEd to $APPLICATIONS/$TRED_DIR ..."
 
 elif [ ! -z $INSTALL_SCRIPT_DIR  ]; then
-	$INSTALL_SCRIPT --tred-dir "/Applications/$TRED_DIR" || exit 4
+	$INSTALL_SCRIPT --tred-dir "$APPLICATIONS/$TRED_DIR" || exit 4
 else
-	$INSTALL_SCRIPT -L $INSTALL_SCRIPT_DIR --tred-dir "/Applications/$TRED_DIR" || exit 4
+	$INSTALL_SCRIPT -L $INSTALL_SCRIPT_DIR --tred-dir "$APPLICATIONS/$TRED_DIR" || exit 4
 fi
 
 # The code needs to be signed so it runs on Mountain Lion without obstacles
 echo "Sign the code with UFAL certificate ..."
 echo "\t> SKIPPING"
 # security unlock-keychain -p tred
-# codesign -f -s "$SIGN_CERT_SHA1" -r='designated => anchor apple generic and identifier "cz.cuni.mff.ufal.tred"' -v "/Applications/$TRED_APP" || exit 5
+# codesign -f -s "$SIGN_CERT_SHA1" -r='designated => anchor apple generic and identifier "cz.cuni.mff.ufal.tred"' -v "$APPLICATIONS/$TRED_APP" || exit 5
 
 
 # Copy installed TrEd and its signature to the release image
-echo "Copying /Applications/$TRED_DIR to $MOUNT_POINT/$TRED_DIR ..."
-cp -R "/Applications/$TRED_DIR" "$MOUNT_POINT/$TRED_DIR" || exit 6
-cp -R "/Applications/$TRED_INSTALL_APP/Contents/_CodeSignature" "$MOUNT_POINT/$TRED_APP/Contents/_CodeSignature" || exit 6
-cp "/Applications/$TRED_INSTALL_APP/Contents/MacOS/TrEd" "$MOUNT_POINT/$TRED_APP/Contents/MacOS/TrEd" || exit 6
+echo "Copying $APPLICATIONS/$TRED_DIR to $MOUNT_POINT/$TRED_DIR ..."
+cp -R "$APPLICATIONS/$TRED_DIR" "$MOUNT_POINT/$TRED_DIR" || exit 6
+cp -R "$APPLICATIONS/$TRED_INSTALL_APP/Contents/_CodeSignature" "$MOUNT_POINT/$TRED_APP/Contents/_CodeSignature" || exit 6
+cp "$APPLICATIONS/$TRED_INSTALL_APP/Contents/MacOS/TrEd" "$MOUNT_POINT/$TRED_APP/Contents/MacOS/TrEd" || exit 6
 
 
 # Finalize the release dmg file
