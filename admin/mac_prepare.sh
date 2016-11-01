117b98ce778e11b444037c68867dbe2c6d1dbf6e
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2016-11-01 16:02:26 +0000

    fixing readonly dmg

--- admin/mac_prepare.sh
+++ admin/mac_prepare.sh
@@ -7,6 +7,6 @@ EOF
 scp ${WWW}/tred/install_tred.bash $MAC_RELEASER:$MAC_TRED_INSTALLATION
 scp ${WWW}/tred/tred-current.tar.gz $MAC_RELEASER:$MAC_TRED_INSTALLATION
 ssh $MAC_RELEASER <<EOF
-	pwd
 	cd TrEd && svn update || echo "Unable to update repository. Do it manually if needed!!!\nContinues without repository updated.\n"
+	cd TrEd/trunk/osx_install/bin && ./unpack.sh && mv release-template.dmg ../scripts
 EOF
