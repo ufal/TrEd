eeb487bb99683ec7aa113ac482ea5067cd118afc
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2016-11-01 16:39:30 +0000

    added execute permition

--- admin/mac_prepare.sh
+++ admin/mac_prepare.sh
@@ -7,6 +7,7 @@ EOF
 scp ${WWW}/tred/install_tred.bash $MAC_RELEASER:$MAC_TRED_INSTALLATION
 scp ${WWW}/tred/tred-current.tar.gz $MAC_RELEASER:$MAC_TRED_INSTALLATION
 ssh $MAC_RELEASER <<EOF
+    chmod +x ${WWW}/tred/install_tred.bash
 	cd TrEd && svn update || echo "Unable to update repository. Do it manually if needed!!!\nContinues without repository updated.\n"
 	cd ~/TrEd/trunk/osx_install/bin && ./unpack.sh && mv release-template.dmg ../scripts
 EOF
