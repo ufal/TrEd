33d4bde43eec63e69297519b58448cb4df71beaf
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2016-11-01 16:04:44 +0000

    typo

--- admin/mac_prepare.sh
+++ admin/mac_prepare.sh
@@ -8,5 +8,5 @@ scp ${WWW}/tred/install_tred.bash $MAC_RELEASER:$MAC_TRED_INSTALLATION
 scp ${WWW}/tred/tred-current.tar.gz $MAC_RELEASER:$MAC_TRED_INSTALLATION
 ssh $MAC_RELEASER <<EOF
 	cd TrEd && svn update || echo "Unable to update repository. Do it manually if needed!!!\nContinues without repository updated.\n"
-	cd TrEd/trunk/osx_install/bin && ./unpack.sh && mv release-template.dmg ../scripts
+	cd ~/TrEd/trunk/osx_install/bin && ./unpack.sh && mv release-template.dmg ../scripts
 EOF
