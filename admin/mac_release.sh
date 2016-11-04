bee7f3c1364aac6a2893906fd4fd46fa7c952633
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2016-11-04 09:05:15 +0000

    added usege of perlbrew when releasing

--- admin/mac_release.sh
+++ admin/mac_release.sh
@@ -3,5 +3,9 @@
 . env.sh
 
 ssh $MAC_RELEASER <<EOF
+    . ~/.bash_profile
+    echo "======== perl version"
+    perlbrew info
+    echo "========"
 	cd $MAC_SVN_DIR/trunk/osx_install/scripts && ./make-release.sh
 EOF
