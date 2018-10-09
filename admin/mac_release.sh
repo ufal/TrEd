899cf87ac0ceea34c29943f2b3e4e496812fd3a2
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2018-10-09 12:02:02 +0000

    remove perlbrew from mac release process

--- admin/mac_release.sh
+++ admin/mac_release.sh
@@ -5,9 +5,9 @@ SIGNATURE=$1
 . env.sh
 
 ssh $MAC_RELEASER <<EOF
-    . ~/.bash_profile
-    echo "======== perl version"
-    perlbrew info
-    echo "========"
+  #  . ~/.bash_profile
+  #  echo "======== perl version"
+  #  perlbrew info
+  #  echo "========"
 	cd $MAC_SVN_DIR/trunk/osx_install/scripts && ./make-release.sh $SIGNATURE
 EOF
