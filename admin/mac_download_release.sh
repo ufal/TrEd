aa9ae5b79dff3085c7a67d72786edc1db995824c
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2018-11-16 09:27:20 +0000

    backuping old osx versions to tred/releases/osx/tred_VERSION.dmg

--- admin/mac_download_release.sh
+++ admin/mac_download_release.sh
@@ -2,5 +2,12 @@
 
 . env.sh
 
-scp $MAC_RELEASER:$MAC_TRED_INSTALLATION/tred.dmg ${WWW}/tred
+SVN_VERSION=`svn info . | grep 'Revision:' | sed -E 's/[^0-9]+//g'`
 
+echo "Copying tred.dmg to local www"
+scp $MAC_RELEASER:$MAC_TRED_INSTALLATION/tred.dmg "${TREDWWW}/tred/"
+
+# keep history of released versions in subdir releases/osx
+mkdir -p "${TREDWWW}/releases/osx" 2>/dev/null
+DMG_FILE="tred_2.${SVN_VERSION}.dmg"
+cp "${TREDWWW}/tred/tred.dmg" "${TREDWWW}/tred/releases/osx/$DMG_FILE"
