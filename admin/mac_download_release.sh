40da8faced8f33b6b0220481553c3772b29cf5f4
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2018-11-21 06:48:11 +0000

    fox osx releases path

--- admin/mac_download_release.sh
+++ admin/mac_download_release.sh
@@ -8,6 +8,6 @@ echo "Copying tred.dmg to local www"
 scp $MAC_RELEASER:$MAC_TRED_INSTALLATION/tred.dmg "${TREDWWW}/tred/"
 
 # keep history of released versions in subdir releases/osx
-mkdir -p "${TREDWWW}/releases/osx" 2>/dev/null
+mkdir -p "${TREDWWW}/tred/releases/osx" 2>/dev/null
 DMG_FILE="tred_2.${SVN_VERSION}.dmg"
 cp "${TREDWWW}/tred/tred.dmg" "${TREDWWW}/tred/releases/osx/$DMG_FILE"
