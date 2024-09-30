8fddd1cbae6f67c3868c8328fa2770320f8996c6
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2024-09-30 15:49:09 +0200

    use perlbrew in mac releaser (the default perlbrew perl !!!)

--- admin/mac_release.sh
+++ admin/mac_release.sh
@@ -5,9 +5,9 @@ SIGNATURE=$1
 . env.sh
 
 ssh $MAC_RELEASER <<EOF
-  #  . ~/.bash_profile
-  #  echo "======== perl version"
-  #  perlbrew info
-  #  echo "========"
+  . ~/perl5/perlbrew/etc/bashrc
+  echo "======== perl version"
+  perlbrew info
+  echo "========"
 	cd ${MAC_GIT_DIR}/osx_install/scripts && ./make-release.sh $SIGNATURE
 EOF
